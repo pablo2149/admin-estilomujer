9059062800440eee0aea507c75d18fc1347b1ef2
Estilo <coria580@gmail.com>
2025-05-05 17:46:03 -0300

    Corrigiendo rutas del panel

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,25 @@
-import React from 'react';
-import './App.css';
-import Login from './Login';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Dashboard from './Dashboard';
-import Clientas from './Clientas';
-import Canjes from './Canjes';
-import Premios from './Premios';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Login from "./Login";
+import Dashboard from "./pages/Dashboard";
+import Clientas from "./pages/Clientas";
+import Premios from "./pages/Premios";
+import CrearPremio from "./pages/CrearPremio";
+import Historial from "./pages/Historial";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <Router>
-        <Routes>
-          <Route path="/" element={<Login />} />
-          <Route path="/dashboard" element={<Dashboard />} />
-          <Route path="/clientas" element={<Clientas />} />
-          <Route path="/canjes" element={<Canjes />} />
-          <Route path="/premios" element={<Premios />} />
-        </Routes>
-      </Router>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/clientas" element={<Clientas />} />
+        <Route path="/premios" element={<Premios />} />
+        <Route path="/crear-premio" element={<CrearPremio />} />
+        <Route path="/historial" element={<Historial />} />
+      </Routes>
+    </Router>
   );
 }
 
